5e1e557508790b3d472ce95767fb44cdce9c548a
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-02-22 22:25:00 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
+  turn = 0
   board = ["#{board[0]}", "#{board[1]}", "#{board[2]}", "#{board[3]}", "#{board[4]}", "#{board[5]}", "#{board[6]}", "#{board[7]}", "#{board[8]}"]
   board.each do |each_turn|
-    turn = 0
 if each_turn == "X" || each_turn == "O"
   turn += 1
 end
