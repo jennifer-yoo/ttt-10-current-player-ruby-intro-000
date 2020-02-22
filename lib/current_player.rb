6c777d8df73aab2aa46c8a9a551b8843da0501d4
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-02-22 22:17:26 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,8 @@
 def turn_count(board)
   turn_total = ["#{board[0]}", "#{board[1]}", "#{board[2]}", "#{board[3]}", "#{board[4]}", "#{board[5]}", "#{board[6]}", "#{board[7]}", "#{board[8]}"]
-  turn_total.each do |each_turn|
+  board.each do |each_turn|
     turn = 0
-if each_turn = "X" || each_turn = "O"
+if each_turn == "X" || each_turn == "O"
   turn += 1
   return turn
 end
